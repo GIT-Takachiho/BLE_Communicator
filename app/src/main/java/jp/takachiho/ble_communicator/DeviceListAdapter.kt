306980267143e7984802f4384cdfca175ca5d0db
GIT-Takachiho <okabe@takachiho-sc.co.jp>
2022-05-10 16:59:25 +0900

    202205101659

---
+++ app/src/main/java/jp/takachiho/ble_communicator/DeviceListAdapter.kt
@@ -0,0 +1,74 @@
+package jp.takachiho.ble_communicator
+
+import android.bluetooth.BluetoothDevice
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.TextView
+import java.util.*
+
+
+class DeviceListAdapter(context: Context) : BaseAdapter(){
+
+    private var mDeviceList: ArrayList<BluetoothDevice>? = null
+    private var mInflator: LayoutInflater? = null
+
+    // リストへの追加
+    fun addDevice(device: BluetoothDevice) {
+        if (!mDeviceList!!.contains(device)) {    // 加えられていなければ加える
+            mDeviceList!!.add(device)
+            notifyDataSetChanged() // ListViewの更新
+        }
+    }
+
+    // リストのクリア
+    fun clear() {
+        mDeviceList!!.clear()
+        notifyDataSetChanged() // ListViewの更新
+    }
+
+    override fun getCount(): Int {
+        return mDeviceList!!.size
+    }
+
+    override fun getItem(position: Int): Any {
+        return mDeviceList!![position]
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    internal class ViewHolder {
+        var deviceName: TextView? = null
+        var deviceAddress: TextView? = null
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View? {
+        var convertview = convertView
+        val viewHolder: ViewHolder
+        // General ListView optimization code.
+        if (null == convertview) {
+            convertview = mInflator!!.inflate(R.layout.listitem_device, parent, false)
+            viewHolder = ViewHolder()
+            viewHolder.deviceAddress =
+                convertview.findViewById<View>(R.id.textview_deviceaddress) as TextView
+            viewHolder.deviceName =
+                convertview.findViewById<View>(R.id.textview_devicename) as TextView
+            convertview.setTag(viewHolder)
+        } else {
+            viewHolder = convertview.tag as ViewHolder
+        }
+        val device = mDeviceList!![position]
+        val deviceName = device.name
+        if (null != deviceName && 0 < deviceName.length) {
+            viewHolder.deviceName!!.setText(deviceName)
+        } else {
+            viewHolder.deviceName!!.setText(R.string.unknown_device)
+        }
+        viewHolder.deviceAddress!!.setText(device.address)
+        return convertview
+    }
+}
---
+++ app/src/main/java/jp/takachiho/ble_communicator/MainActivity.kt
@@ -0,0 +1,134 @@
+package jp.takachiho.ble_communicator
+
+//import BluetoothConnect
+//import android.bluetooth.le.BluetoothLeScanner
+//import android.bluetooth.le.ScanCallback
+//import android.bluetooth.le.ScanResult
+
+//import android.location.LocationManager
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+//import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+//import kotlinx.android.synthetic.main.activity_device_list.*
+//import kotlinx.android.synthetic.main.activity_main.view.*
+
+
+//import kotlinx.android.synthetic.main.activity_main.*
+
+//var mBluetoothLeScanner: BluetoothLeScanner? = null
+//var mScanCallback: ScanCallback? = null
+
+class MainActivity : AppCompatActivity() {
+
+    // 定数
+    // Bluetooth機能の有効化要求時の識別コード
+    private val REQUEST_ENABLEBLUETOOTH = 1
+    // デバイス接続要求時の識別コード
+    private val REQUEST_CONNECTDEVICE = 2
+
+
+    // メンバー変数
+    // BluetoothAdapter : Bluetooth処理で必要
+    private var mBluetoothAdapter : BluetoothAdapter? = null
+    // デバイスアドレス
+    private var mDeviceAddress = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Android端末がBLEをサポートしてるかの確認
+        if (!packageManager.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_is_not_supported, Toast.LENGTH_SHORT).show()
+            finish() // アプリ終了宣言
+            return
+        }
+
+        // Bluetoothアダプタの取得
+        val bluetoothManager = getSystemService(BLUETOOTH_SERVICE) as BluetoothManager
+        mBluetoothAdapter = bluetoothManager.adapter
+        if (null == mBluetoothAdapter) {    // Android端末がBluetoothをサポートしていない
+            Toast.makeText(this, R.string.bluetooth_is_not_supported, Toast.LENGTH_SHORT).show()
+            finish() // アプリ終了宣言
+            return
+        }
+    }
+/*
+    // 機能の有効化ダイアログの操作結果
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        when (requestCode) {
+            REQUEST_ENABLEBLUETOOTH -> if (RESULT_CANCELED == resultCode) {    // 有効にされなかった
+                Toast.makeText(this, R.string.bluetooth_is_not_working, Toast.LENGTH_SHORT).show()
+                finish() // アプリ終了宣言
+                return
+            }
+            REQUEST_CONNECTDEVICE -> {
+                val strDeviceName: String?
+                if (RESULT_OK == resultCode) {
+                    // デバイスリストアクティビティからの情報の取得
+                    if (data != null) {
+//                        strDeviceName = data.getStringExtra(DeviceListActivity.EXTRAS_DEVICE)
+                    }
+                    if (data != null) {
+//                        mDeviceAddress = data.getStringExtra(DeviceListActivity.EXTRAS_DEVICE_ADDRESS)!!
+                    }
+                } else {
+                    strDeviceName = ""
+                    mDeviceAddress = ""
+                }
+//                (findViewById(R.id.textview_devicename) as TextView).text = strDeviceName
+                (findViewById(R.id.textview_deviceaddress) as TextView).text = mDeviceAddress
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+ */
+
+    // 初回表示時、および、ポーズからの復帰時
+    override fun onResume() {
+        super.onResume()
+
+        // Android端末のBluetooth機能の有効化要求
+        requestBluetoothFeature()
+    }
+
+    // Android端末のBluetooth機能の有効化要求
+    private fun requestBluetoothFeature() {
+        if (mBluetoothAdapter!!.isEnabled) {
+            return
+        }
+        // デバイスのBluetooth機能が有効になっていないときは、有効化要求（ダイアログ表示）
+        val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+        startActivityForResult(enableBtIntent, REQUEST_ENABLEBLUETOOTH)
+    }
+
+
+    // オプションメニュー作成時の処理
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main, menu)
+        return true
+    }
+
+    // オプションメニューのアイテム選択時の処理
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menuitem_search -> {
+                val devicelistactivityIntent = Intent(this, DeviceListActivity::class.java)
+                startActivityForResult(devicelistactivityIntent, REQUEST_CONNECTDEVICE)
+                return true
+            }
+        }
+        return false
+    }
+
+}
+
